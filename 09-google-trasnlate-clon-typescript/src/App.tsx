eb9c0ebde63b237d47e7dc4e6a2f0d69157b169e
FacundoBe <facundobellomo@gmail.com>
2024-02-14 16:10:59 -0300

    comenzando con el reducer

--- 09-google-trasnlate-clon-typescript/src/App.tsx
+++ 09-google-trasnlate-clon-typescript/src/App.tsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
+import { useReducer, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// 1 create initial state
+const initial_state = {
+  fromLaguage: 'auto',
+  toLanguage: 'en',
+  fromTexto: '',
+  result: '',
+  loading: false
+}
 
+// 2 create reducer
+function reducer (state, action) {
+const { type } = action
+}
+
+
+function App() {
+  // usar el useReducer
+  const [state, dispatch] = useReducer(reducer, initial_state)
   return (
     <>
-     <h1>Translator clon</h1>
+      <h1>Translator clon</h1>
     </>
   )
 }
