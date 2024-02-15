e6c4939e1815654ea037d6f75c70b71c0d9104fe
FacundoBe <facundobellomo@gmail.com>
2024-02-15 17:00:17 -0300

    avanzando traductor

--- 09-google-trasnlate-clon-typescript/src/App.tsx
+++ 09-google-trasnlate-clon-typescript/src/App.tsx
@@ -1,28 +1,75 @@
-import { useReducer, useState } from 'react'
+import { useReducer } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { type Action, type State } from './types'
 
 // 1 create initial state
-const initial_state = {
-  fromLaguage: 'auto',
+const initial_state: State = {
+  fromLanguage: 'auto',
   toLanguage: 'en',
-  fromTexto: '',
+  fromText: '',
   result: '',
   loading: false
 }
 
 // 2 create reducer
-function reducer (state, action) {
-const { type } = action
+function reducer(state: State, action: Action) {
+  const { type } = action
+
+  if (type === 'INTERCHANGE_LANGUAGUES') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    return {
+      ...state,
+      toLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      fromText: action.payload,
+      loading: true,
+      result: ''
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      loading: false,
+      result: action.payload
+    }
+  }
+
+  return state //si el type no coincide con las acciones programadas, devuelve el estado sin modificar
 }
 
 
 function App() {
   // usar el useReducer
   const [state, dispatch] = useReducer(reducer, initial_state)
+
   return (
     <>
-      <h1>Translator clon</h1>
+      <h1>Translator clon </h1>
+      <button onClick={() => dispatch({ type: 'SET_FROM_LANGUAGE', payload: 'ingles' })}>
+        Cambiar From language
+      </button>
+      <h2>{state.fromLanguage}</h2>
     </>
   )
 }
