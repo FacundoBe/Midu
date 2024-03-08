15641b95329e8bb87026c493cab44b82677bcbed
FacundoBe <facundobellomo@gmail.com>
2024-03-08 02:05:21 -0300

    en proyecto 11 tabla con formato mostrando users lista

--- 11_Ptecnica_tabla_usrs/src/App.jsx
+++ 11_Ptecnica_tabla_usrs/src/App.jsx
@@ -6,6 +6,7 @@ import Table from './components/Table'
 
 function App() {
   const [users, setUsers] = useState([])
+  const [filasColor, setFilasColor] = useState(false)
 
   useEffect(() => {
     fetch("https://randomuser.me/api/?results=100")
@@ -18,10 +19,20 @@ function App() {
   }
     , [])
 
+  function handleDelete(uuid) {
+    setUsers(prevUsers => prevUsers.filter(user => user.login.uuid !== uuid))
+  }
+
   return (
     <>
-      <h1>Users Table</h1>
-      <Table users={users}/>
+      <h1>Lista de Usuarios</h1>
+      <div>
+        <button onClick={() => setFilasColor(prevCol => !prevCol)}> Colorea filas </button>
+      </div>
+      <Table users={users}
+        handleDelete={handleDelete}
+        filasColor={filasColor}
+      />
     </>
   )
 }
--- 11_Ptecnica_tabla_usrs/src/components/Table.jsx
+++ 11_Ptecnica_tabla_usrs/src/components/Table.jsx
@@ -1,17 +1,26 @@
-export default function Table({ users }) {
+export default function Table({ users, handleDelete, filasColor  }) {
+
+    const style1 = {backgroundColor: '#8a8787'}
+    const style2 = {backgroundColor: '#525151'}
 
     return (
         <table>
             <tr>
-                <th> Foto</th>
+                <th> <strong>Foto</strong></th>
                 <th> Nombre</th>
                 <th> Apellido</th>
                 <th> Pais</th>
                 <th> Acción</th>
             </tr>
-            {users.map(user => {
-                <h2>hola</h2>
-                        }
+            {users.map((user, index) => {
+                return (<tr key={user.login.uuid} style= {filasColor ? (index%2===0 ? style1 : style2) : null}> 
+                    <td> <img src={user.picture.thumbnail} alt="" /></td>
+                    <td> {user.name.first}</td>
+                    <td>{user.name.last}</td>
+                    <td> {user.location.country}</td>
+                    <td> <button id={user.login.uuid} onClick={()=> handleDelete(user.login.uuid)}>Delete</button></td>
+                </tr>)
+            }
             )}
 
         </table>
