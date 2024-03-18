479cd8c8a778be7366a77b2d9551c856d2f5b1bf
FacundoBe <facundobellomo@gmail.com>
2024-03-17 22:26:14 -0300

    prueba 11 con scroll infinito automatico o a boton

--- 11_Ptecnica_tabla_usrs/src/App.jsx
+++ 11_Ptecnica_tabla_usrs/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState, useMemo } from 'react'
 import './App.css'
 import Table from './components/Table'
+import { useIntersectionObserver } from 'usehooks-ts'
+
 
 function App() {
   const [users, setUsers] = useState([])
@@ -11,13 +13,19 @@ function App() {
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
   const [currentPage, setCurrenrPage] = useState(1)
+  const [scrollType, setScrollType] = useState('boton')
+
+  const { isIntersecting, ref } = useIntersectionObserver({}) //para saber cuando el div al final
+  // de la tabla es visible y ahi cargar mas datos
+
+  console.log(isIntersecting)
 
   async function fetchUsers() {
     return await fetch(`https://randomuser.me/api/?results=10&seed=midudev&page=${currentPage}`)
-    .then(res => {
-      if (res.ok) return res.json()
-      else throw new Error(res.status)
-    }).then(data => data.results)
+      .then(res => {
+        if (res.ok) return res.json()
+        else throw new Error(res.status)
+      }).then(data => data.results)
   }
 
   useEffect(() => {
@@ -40,6 +48,16 @@ function App() {
   }
     , [currentPage])
 
+
+  useEffect(() => {  // activa el infinity scroll si el div al final de la tabla esta a la vista
+    if (users.length > 0 && isIntersecting && scrollType === 'infi') {
+      setCurrenrPage(prevPge => prevPge + 1)
+    }
+  }, [isIntersecting, scrollType])
+
+  function handleScrollType () {
+    setScrollType(prevType => prevType === 'boton' ? 'infi' : 'boton')
+  }
 
   function handleDelete(uuid) {
     setUsers(prevUsers => prevUsers.filter(user => user.login.uuid !== uuid))
@@ -84,34 +102,37 @@ function App() {
     <>
       <h1>Lista de Usuarios</h1>
       <header>
-        <button
-          className={filasColor ? 'button-selected' : ""}
-          onClick={() => setFilasColor(prevCol => !prevCol)}
-        >
-          Colorea filas
-        </button>
-        <button
-          onClick={handleOrderByCountry}
-          style={{ width: '150px' }}
-        >
-          {orderBy === 'country' ? 'No ordenar por pais' : 'Ordenar por Pais'}
-        </button>
-        <button onClick={() => setUsers(originalUsers.current)}>
-          Restaurar estado inicial
-        </button>
-        <div className='filter-div'>
-          <input placeholder='Filtro por pais'
-            size={10}
-            type="text"
-            onChange={handleFilterCountry}
-            value={filterCountry}
-          />
-          <button className='clear-filter' onClick={() => setFilterCountry('')}>x</button>
+        <div className='main-header'>
+          <button
+            className={filasColor ? 'button-selected' : ""}
+            onClick={() => setFilasColor(prevCol => !prevCol)}
+          >
+            Colorea filas
+          </button>
+          <button
+            onClick={handleOrderByCountry}
+            style={{ width: '150px' }}
+          >
+            {orderBy === 'country' ? 'No ordenar por pais' : 'Ordenar por Pais'}
+          </button>
+          <button onClick={() => setUsers(originalUsers.current)}>
+            Restaurar estado inicial
+          </button>
+          <div className='filter-div'>
+            <input placeholder='Filtro por pais'
+              size={10}
+              type="text"
+              onChange={handleFilterCountry}
+              value={filterCountry}
+            />
+            <button className='clear-filter' onClick={() => setFilterCountry('')}>x</button>
+          </div>
         </div>
+        <button onClick={handleScrollType}>
+          Scroll: {scrollType === 'infi' ? 'infinity' : 'Con boton'}
+        </button>
       </header>
       <main>
-
-
 
         {users.length > 0 && <Table users={sortedUsers}
           handleDelete={handleDelete} filasColor={filasColor}
@@ -121,9 +142,11 @@ function App() {
 
         {!loading && error && users.length === 0 && <p>Se produjo un error al cargar los datos </p>}
 
-        {!loading && !error && <button onClick={() => setCurrenrPage(prevCurrPage => prevCurrPage + 1)}>
+        {!loading && !error && scrollType === 'boton' && <button onClick={() => setCurrenrPage(prevCurrPage => prevCurrPage + 1)}>
           Cargar mas resultados
         </button>}
+
+        <div ref={ref}></div>
 
       </main>
     </>
